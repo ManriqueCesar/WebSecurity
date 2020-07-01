8ee5c349edde7f5f19828562f89a289e8961dfe5
astutox <38671208+astutox@users.noreply.github.com>
2020-06-30 22:10:06 -0500

    Código depurado y correción de bugs

--- WebSecurity/DESARROLLO/PWCEV/Front/web/script/newTest.js
+++ WebSecurity/DESARROLLO/PWCEV/Front/web/script/newTest.js
@@ -1,66 +1,4 @@
-$(document).ready(function() {
-
-
-
-var defaultTítulo = 'Ingresar el título del examen';
-
-function endEdit(e) {
-  var input = $(e.target),
-      label = input && input.prev();
-
-  label.text(input.val() === '' ? defaultTítulo : input.val());
-  input.hide();
-  label.show();
-}
-
-$('.clickedit').hide()
-.focusout(endEdit)
-.keyup(function (e) {
-  if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-      endEdit(e);
-      return false;
-  } else {
-      return true;
-  }
-})
-.prev().click(function () {
-  $(this).hide();
-  $(this).next().show().focus();
-});
-
-var defaultDescripción = 'Ingresar la descripción';
-
-function endEdit2(e) {
-  var input = $(e.target),
-      label = input && input.prev();
-
-  label.text(input.val() === '' ? defaultDescripción : input.val());
-  input.hide();
-  label.show();
-}
-
-$('.clickedit2').hide()
-.focusout(endEdit2)
-.keyup(function (e) {
-  if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-      endEdit(e);
-      return false;
-  } else {
-      return true;
-  }
-})
-.prev().click(function () {
-  $(this).hide();
-  $(this).next().show().focus();
-});
-
-
-
-
-
-
-
-$(function () {
+function cargarFecha() {
   //Initialize Select2 Elements
   //Initialize Select2 Elements
   console.log("asdasd");
@@ -89,75 +27,154 @@ $(function () {
       format: 'MM/DD/YYYY hh:mm A'
     }
   })
-  //Date range as a button
-  $('#daterange-btn').daterangepicker({
-      ranges: {
-        'Today': [moment(), moment()],
-        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-        'This Month': [moment().startOf('month'), moment().endOf('month')],
-        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf(
-          'month')]
-      },
-      startDate: moment().subtract(29, 'days'),
-      endDate: moment()
-    },
-    function (start, end) {
-      $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-    }
-  )
-
   //Timepicker
   $('#timepicker').datetimepicker({
     format: 'LT'
   })
 
-})
+}
 
-var uniqueId = 1;
-$(function() {
-     $('.addRow').click(function() {
+function editarTitulo() {
+  var defaultTítulo = 'Ingresar el título del examen';
 
-      //$("#original").clone().appendTo("#containers");
-         var copy = $("#original").clone(true);
-         var formId = 'NewForm' + uniqueId;
-         copy.attr('id', formId );
-         $('#campaign').append(copy);
-         $('#' + formId).find('input,select').each(function(){
-            $(this).attr('id', $(this).attr('id') + uniqueId); 
-             
-         });
-         uniqueId++; 
-     });
-});
+  function endEdit(e) {
+    var input = $(e.target),
+      label = input && input.prev();
 
-contador = 0;
-
-
-
-
-$('#close').click(function () {
-  console.log("asd1");
-  var id = $(this).closest("form")
-  console.log(id)
-  $( ".NewForm" ).remove();
-  console.log("asd2");
-});
-
-
-
-
-});
-
-
-$('.addRow').mousedown(function() {
-  contador++;
-  console.log(contador)
-  if(contador>9){
-    console.log('maximo')
-    $('.addRow').prop('disabled', true);
-
+    label.text(input.val() === '' ? defaultTítulo : input.val());
+    input.hide();
+    label.show();
   }
-  //$( this ).append( "<span style='color:#00f;'>Mouse down.</span>" );
+
+  $('.clickedit').hide()
+    .focusout(endEdit)
+    .keyup(function (e) {
+      if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+        endEdit(e);
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .prev().click(function () {
+      $(this).hide();
+      $(this).next().show().focus();
+    });
+}
+
+function editarDescripcion() {
+  var defaultDescripción = 'Ingresar la descripción';
+
+  function endEdit2(e) {
+    var input = $(e.target),
+      label = input && input.prev();
+
+    label.text(input.val() === '' ? defaultDescripción : input.val());
+    input.hide();
+    label.show();
+  }
+
+  $('.clickedit2').hide()
+    .focusout(endEdit2)
+    .keyup(function (e) {
+      if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+        endEdit(e);
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .prev().click(function () {
+      $(this).hide();
+      $(this).next().show().focus();
+    });
+
+}
+
+function duplicarAlternativas(unique) {
+
+    console.log("duplicandoAlternativas");
+    //$("#original").clone().appendTo("#containers");
+    var copy = $("#alternativas").clone(true);
+    var formId = 'alternativa' + unique;
+    copy.attr('id', formId);
+    $('#bloque').append(copy);
+    $('#' + formId).find('input,select').each(function () {
+      $(this).attr('id', $(this).attr('id') + unique);
+
+    });
+    
+
+}
+
+function duplicar(uniqueId, unique) {
+  duplicarAlternativas(unique);
+  unique++;
+  //$("#original").clone().appendTo("#containers");
+
+  var copy = $("#original").clone(true);
+  var formId = 'NewForm' + uniqueId;
+  copy.attr('id', formId);
+  $('#campaign').append(copy);
+  $('#' + formId).find('input,select').each(function () {
+    $(this).attr('id', $(this).attr('id') + uniqueId);
+
+  });
+
+}
+
+
+
+
+
+function validarRadio() {
+  // Uncheck radio buttons in #group1 and #group2
+  $('#group1 [type=radio]:checked, #group2 [type=radio]:checked').prop('checked', false).checkboxradio('refresh');
+  // "OR" to select all radio buttons in DOM
+  $('[type=radio]:checked').prop('checked', false).checkboxradio('refresh');
+  // Check clicked radio
+  $(this).prop('checked', true).checkboxradio('refresh');
+}
+
+$(document).ready(function () {
+  cargarFecha();
+  editarTitulo();
+  editarDescripcion();
+  var uniqueId = 1;
+  var unique = 7;
+  var contador = 0;
+  //funcion para duplicar preguntas
+
+  $('.addRow').click(function () {
+
+    duplicar(uniqueId, unique);
+
+    uniqueId++;
+    unique++;
+
+    contador++;
+    if (contador > 8) {
+      console.log('maximo')
+      $('.addRow').prop('disabled', true);
+    }
+  });
+
+  $(document).on('change', '[type=radio]', function (e) {});
+
+
+
+  $('#close').click(function () {
+    console.log("asd1");
+    var id = $(this).closest("form")
+    console.log(id)
+    $(".NewForm").remove();
+    console.log("asd2");
+  });
+
+  $('#params').click(function () {
+    var titulo = $("#txtTitulo").text();
+    console.log(titulo);
+    $('#inputTitulo').val(titulo);
+  });
+
 });
