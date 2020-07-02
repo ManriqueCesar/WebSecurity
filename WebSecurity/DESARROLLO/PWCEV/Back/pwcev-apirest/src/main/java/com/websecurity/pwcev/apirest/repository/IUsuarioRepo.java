c7770cee142a7a9deca5f3c1724b7b3fbf5613b8
Gabriela Laurel <gabriela.laurel>
2020-07-02 18:05:45 -0500

    validacion de usuario por username y password

--- WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/repository/IUsuarioRepo.java
+++ WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/repository/IUsuarioRepo.java
@@ -1,5 +1,7 @@
 package com.websecurity.pwcev.apirest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.websecurity.pwcev.apirest.model.Usuario;
@@ -9,4 +11,8 @@ public interface IUsuarioRepo extends JpaRepository<Usuario, Integer>{
 	Usuario findOneByUsername(String username);
 	
 	Usuario findOneByEmail(String email);
+	
+    Optional<Usuario> findByUsernameAndPassword(String username,String password);
+	
+	boolean existsByUsernameAndPassword(String username, String password);
 }
--- WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/service/impl/UserServiceImpl.java
+++ WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/service/impl/UserServiceImpl.java
@@ -1,8 +1,52 @@
 package com.websecurity.pwcev.apirest.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.websecurity.pwcev.apirest.model.Rol;
+import com.websecurity.pwcev.apirest.model.Usuario;
+import com.websecurity.pwcev.apirest.repository.IUsuarioRepo;
+import com.websecurity.pwcev.apirest.service.IUsuarioService;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements IUsuarioService{
+	@Autowired
+	private IUsuarioRepo usuarioRepositorio;
+
+
+
+	public boolean existeUsuarioByUserPass(String username, String password) {
+		
+		boolean respuesta = usuarioRepositorio.existsByUsernameAndPassword(username, password);
+		return respuesta;
+	}
+	@Override
+	public Optional<Usuario> buscarPorUsurnamePassword(String username, String password) {
+		
+		
+		
+		Optional<Usuario> us = usuarioRepositorio.findByUsernameAndPassword(username,password);
+	
+		
+	
+	
+	return us;
+	}
+	@Override
+	public List<Usuario> listar() {
+		List<Usuario> users = usuarioRepositorio.findAll();
+		return users;
+	}
+	@Override
+	public Optional<Usuario> buscarPorId(int id) {
+		
+		return usuarioRepositorio.findById(id) ;
+	}
+	
+
 
 }
