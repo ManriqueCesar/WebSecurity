29752691785141987aa2d0595bbd949dc61ab040
Gabriela Laurel <gabriela.laurel>
2020-07-03 00:24:46 -0500

    validacion de usuario por username y password

--- WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/repository/IUsuarioRepo.java
+++ WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/repository/IUsuarioRepo.java
@@ -1,12 +1,19 @@
 package com.websecurity.pwcev.apirest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.websecurity.pwcev.apirest.model.Usuario;
-
+@Repository
 public interface IUsuarioRepo extends JpaRepository<Usuario, Integer>{
 
 	Usuario findOneByUsername(String username);
 	
 	Usuario findOneByEmail(String email);
+	Optional<Usuario> findByUsernameAndPassword(String username,String password);
+	
+	boolean existsByUsernameAndPassword(String username, String password);
+
 }
