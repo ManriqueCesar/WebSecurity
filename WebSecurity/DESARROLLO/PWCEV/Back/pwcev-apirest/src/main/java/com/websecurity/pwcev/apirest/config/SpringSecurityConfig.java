6265efdcc2882c88ca19330d7718c279a7a08a66
Fernando Fuentes <fernando.fuentes@unmsm.edu.pe>
2020-07-20 15:43:13 -0500

    Permitir acceder a swagger sin autenticacion

--- WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/config/SpringSecurityConfig.java
+++ WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/config/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -42,6 +43,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		.and()
 		.csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**"); 
 	}
 
 }
