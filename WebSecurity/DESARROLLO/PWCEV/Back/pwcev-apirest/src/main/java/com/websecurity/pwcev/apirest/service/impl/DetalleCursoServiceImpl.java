712ece6e21dad7069b024dc53bdef120118a6795
GabrielaLaurel <gaby.laquine@gmail.com>
2020-07-26 11:15:29 -0500

    Crear el curso y su detalle, se crea la asociacion automatica entre curso y profesor

--- WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/service/impl/DetalleCursoServiceImpl.java
+++ WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/service/impl/DetalleCursoServiceImpl.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.websecurity.pwcev.apirest.model.Curso;
 import com.websecurity.pwcev.apirest.model.DetalleCurso;
 import com.websecurity.pwcev.apirest.repository.IDetalleCursoRepo;
+import com.websecurity.pwcev.apirest.service.ICursoService;
 import com.websecurity.pwcev.apirest.service.IDetalleCursoService;
 import com.websecurity.pwcev.apirest.service.IUsuarioService;
 
@@ -17,14 +19,22 @@ public class DetalleCursoServiceImpl implements IDetalleCursoService {
 	private IDetalleCursoRepo repo;
 	@Autowired
 	private IUsuarioService usuarioService;
+	@Autowired
+	private ICursoService cursoService ;
+	
+	
 
 	@Override
 	public DetalleCurso registrar(DetalleCurso registro) {
-		boolean existeusuario = usuarioService.existeUsuarioById(registro.getUsuario().getIdUsuario());
 
+		boolean existeusuario = usuarioService.existeUsuarioById(registro.getUsuario().getIdUsuario());
+		Curso curso = new Curso();
+		
 		if (existeusuario) {
 			boolean esprofe = usuarioService.validarRol(registro.getUsuario(), 2);
 			if (esprofe) {
+				curso=cursoService.registrar(registro.getCurso());
+				registro.setCurso(curso);
 				return repo.save(registro);
 			}
 		}
