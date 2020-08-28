38ee02ec30aeaaf5f9bd5faacab7196fe7a69db6
ManriqueCesar <38671208+ManriqueCesar@users.noreply.github.com>
2020-08-27 21:12:43 -0500

    vista MisCursos creada para Alumno

--- WebSecurity/DESARROLLO/PWCEV/Front/web/script/demo.js
+++ WebSecurity/DESARROLLO/PWCEV/Front/web/script/demo.js
@@ -1,32 +1,3 @@
-
-function editarTitulo() {
-  var defaultTítulo = 'Ingresar el título del examen';
-
-  function endEdit(e) {
-    var input = $(e.target),
-      label = input && input.prev();
-
-    label.text(input.val() === '' ? defaultTítulo : input.val());
-    input.hide();
-    label.show();
-  }
-
-  $('.clickedit').hide()
-    .focusout(endEdit)
-    .keyup(function (e) {
-      if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-        endEdit(e);
-        return false;
-      } else {
-        return true;
-      }
-    })
-    .prev().click(function () {
-      $(this).hide();
-      $(this).next().show().focus();
-    });
-}
-
 function Cargar_curso_id(cboid, opcionxdefecto) {
   var idUsuario = Cookies.get('id');
   var ruta = 'https://api-pwcev.herokuapp.com';
@@ -69,7 +40,6 @@ function cbo_universidad(cboid, opcionxdefecto) {
   })
 }
 
-
 function cargarFecha() {
   //Datemask dd/mm/yyyy
   $('.miFecha').datepicker({
@@ -77,9 +47,6 @@ function cargarFecha() {
     language: "es"
   });
 }
-
-
-
 
 function setNombre() {
   var user = Cookies.get('usuario');
@@ -119,7 +86,7 @@ $(document).ready(function () {
   });
 });
 
-
+//Crear curso
 $(document).on('click', '#btn-crear', function (event) {
   var ruta = 'https://api-pwcev.herokuapp.com';
 
@@ -162,24 +129,68 @@ $(document).on('click', '#btn-crear', function (event) {
 
 
 
-$('#btn-close').click(function () {
-  deleteCookie();
+//Crear curso
+$(document).on('click', '#btn-crearExamen', function (event) {
+  var ruta = 'https://api-pwcev.herokuapp.com';
+
+
+  var fechaInicio= ;
+  var horaInicio= ;
+  var tiempoDuracion=  ;
+  var titulo = ;
+
+  var  preguntas=[];
+
+
+  
+
+
+  arregloAlumnos = [];
+  arregloAlumnos = arregloAlumnos.concat(listaAlumnos);
+  console.log(arregloAlumnos);
+  //fin algoritmo
+
+  var curso = {};
+  var request = {};
+  var id = Cookies.get('id');
+  
+  curso.centroEstudios = $('#cbo-centro').val().toUpperCase();
+  curso.curso = $('#txt-curso').val();
+  curso.eap = $('#cbo-eap').val().toUpperCase();
+  curso.periodo = $('#cbo-periodo').val();
+  curso.idCurso = null;
+  request.curso = curso;
+  request.idUsuario = id;
+  request.emailAlumnos = arregloAlumnos;
+  request.idDetalleCurso = null;
+  $.ajax({
+    url: ruta + '/detallecurso/',
+    type: 'POST',
+    dataType: 'json',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify(request)
+  }).done(function () {
+    $('#exampleModalCenter').modal('toggle');
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    console.log("error")
+  })
 });
 
-$('#tabCurso').click(function () {
-  $('.addRow').prop('disabled', true);
-});
+
 
 $('#tabParam').click(function () {
   Cargar_curso_id('cbocurso', 1);
   var options = {
-    now: "12:35", //hh:mm 24 hour format only, defaults to current time 
+    now: "08:00", //hh:mm 24 hour format only, defaults to current time 
     twentyFour: false, //Display 24 hour format, defaults to false 
     upArrow: 'wickedpicker__controls__control-up', //The up arrow class selector to use, for custom CSS 
     downArrow: 'wickedpicker__controls__control-down', //The down arrow class selector to use, for custom CSS 
     close: 'wickedpicker__close', //The close class selector to use, for custom CSS 
     hoverState: 'hover-state', //The hover state class to use, for custom CSS 
-    title: 'Timepicker', //The Wickedpicker's title, 
+    title: 'Horario', //The Wickedpicker's title, 
     showSeconds: false, //Whether or not to show seconds, secondsInterval: 1, //Change interval for seconds, defaults to 1 , 
     minutesInterval: 1, //Change interval for minutes, defaults to 1 
     beforeShow: null, //A function to be called before the Wickedpicker is shown 
@@ -187,56 +198,11 @@ $('#tabParam').click(function () {
     clearable: false, //Make the picker's input clearable (has clickable "x") 
   };
   $('.timepicker').wickedpicker(options);
-
-
-  var titulo = $("#txtTitulo").text();
-  $('#inputTitulo').val(titulo);
   $('.addRow').prop('disabled', true);
 });
 
-$('#tabExamen').click(function () {
-  $('.addRow').prop('disabled', false);
+
+
+$('#btn-close').click(function () {
+  deleteCookie();
 });
-
-$('#pruebatab').click(function () {
-  $('<div>').append($(html).find('a').unwrap()).html();
-});
-
-
-
-$(document).on('click', '#btn-pregunta', function (event) {
-  $('#inputTituloModal').val($("#txtTitulo").text());
-  $('#modalPregunta').modal('toggle');
-  var id = $(this).attr("btn-pregunta", "descripcion")
-
-  //capturar id
-  var labels = document.getElementsByTagName('label');
-  for (var i = 0; i < labels.length; i++) {
-    if (labels[i].htmlFor != '') {
-      var elem = document.getElementById(labels[i].htmlFor);
-      if (elem)
-        elem.label = labels[i];
-
-    }
-  }
-  var miId = $(labels[1]).attr("id");
-  console.log(miId);
-
-
-
-
-  $(this).attr('valor', 'descripcion');
-
-
-
-
-   $(this).attr('id', $(this).attr('id') + uniqueId + '-' + index);
-   $(this).attr('name', $(this).attr('name') + uniqueId);
-  $(this).attr('value', $(this).attr('value') + uniqueId + '-' + index);
-});
-
-
-
-// var descripcion = $("#descripcion").text();
-//  $('#tituloDescripcion').text();
-// console.log($("#descripcion").text())
