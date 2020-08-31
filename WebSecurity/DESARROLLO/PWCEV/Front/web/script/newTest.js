929b27baf7e863cfbbd15ffd2166f535f42842b1
ManriqueCesar <38671208+ManriqueCesar@users.noreply.github.com>
2020-08-31 03:12:59 -0500

    nueva version de newTest implementada: demo.html cambiado a newTest.html

--- WebSecurity/DESARROLLO/PWCEV/Front/web/script/newTest.js
+++ WebSecurity/DESARROLLO/PWCEV/Front/web/script/newTest.js
@@ -40,7 +40,6 @@ function cbo_universidad(cboid, opcionxdefecto) {
   })
 }
 
-
 function cargarFecha() {
   //Datemask dd/mm/yyyy
   $('.miFecha').datepicker({
@@ -49,8 +48,14 @@ function cargarFecha() {
   });
 }
 
+function setNombre() {
+  var user = Cookies.get('usuario');
+  $('#nombreUser').text(user);
+
+}
+
 function editarTitulo() {
-  var defaultTítulo = 'Ingresar el título del examen';
+  var defaultTítulo = 'INGRESAR LA DESCRIPCIÓN DE LA PREGUNTA';
 
   function endEdit(e) {
     var input = $(e.target),
@@ -77,98 +82,69 @@ function editarTitulo() {
     });
 }
 
-function editarDescripcion() {
-  var defaultDescripción = 'Editar la descripción';
-
-  function endEdit2(e) {
-    var input = $(e.target),
-      label = input && input.prev();
-
-    label.text(input.val() === '' ? defaultDescripción : input.val());
-    input.hide();
-    label.show();
-  }
-
-  $('.clickedit2').hide()
-    .focusout(endEdit2)
-    .keyup(function (e) {
-      if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-        endEdit(e);
-        return false;
-      } else {
-        return true;
-      }
-    })
-    .prev().click(function () {
-      $(this).hide();
-      $(this).next().show().focus();
-    });
-
-}
-
-function duplicarAlternativas(unique) {
-  //$("#original").clone().appendTo("#containers");
-  contador = 1;
-  var copy = $("#descripcion").clone(true);
-  var formId = 'opcion' + unique;
-  copy.attr('id', formId);
-  $('#original').append(copy);
-  $('#' + formId).find("p", "label", "input").each(function () {
-    $(this).attr('id', $(this).attr('id') + unique);
-    unique++;
-  });
-  contador++;
-}
-
-function duplicar(uniqueId) {
-  // Clonar
-  var copy = $("#original").clone(true);
-
-  // Setear nuevo id  
-  copy.attr('id', 'NewForm' + uniqueId);
-
-  // Set inputs'id
-  copy.find('label, select').each(function () {
-    $(this).attr('id', $(this).attr('id') + uniqueId);
-    //  $(this).attr('id', $(this).attr('id') + uniqueId + '-' + index);
-    //    $(this).attr('name', $(this).attr('name') + uniqueId);
-    //  $(this).attr('value', $(this).attr('value') + uniqueId + '-' + index);
-  });
-  copy.find('label, select').each(function (index) {
-    $(this).attr('for', $(this).attr('for') + uniqueId + '-' + index);
-  });
-  // Insert new form
-  $('#campaign').append(copy);
-}
-
-function setNombre() {
-  var user = Cookies.get('usuario');
-  $('#nombreUser').text(user);
-
-}
-
 $(document).ready(function () {
   setNombre();
+  editarTitulo();
   cbo_escuela('cbo-eap', 'SOFTWARE');
   cbo_universidad('cbo-centro', 'UNMSM');
-  $('.addRow').prop('disabled', true);
+
   cargarFecha();
-  editarTitulo();
-  editarDescripcion();
+
+
+
+
+
+
+
+  var alternativa11 = $("#alternativa1-1").text("DEMO11");
+  var alternativa12 = $("#alternativa1-2").text("DEMO12");
+  var alternativa13 = $("#alternativa1-3").text("DEMO13");
+  var alternativa14 = $("#alternativa1-4").text("DEMO14");
+  var alternativa15 = $("#alternativa1-5").text("DEMO15");
+
+
+  var alternativa21 = $("#alternativa2-1").text("DEMO21");
+  var alternativa22 = $("#alternativa2-2").text("DEMO22");
+  var alternativa23 = $("#alternativa2-3").text("DEMO23");
+  var alternativa24 = $("#alternativa2-4").text("DEMO24");
+  var alternativa25 = $("#alternativa2-5").text("DEMO25");
+
+  var alternativa31 = $("#alternativa3-1").text("DEMO31");
+  var alternativa32 = $("#alternativa3-2").text("DEMO32");
+  var alternativa33 = $("#alternativa3-3").text("DEMO33");
+  var alternativa34 = $("#alternativa3-4").text("DEMO34");
+  var alternativa35 = $("#alternativa3-5").text("DEMO35");
+
+  var alternativa41 = $("#alternativa4-1").text("DEMO41");
+  var alternativa42 = $("#alternativa4-2").text("DEMO42");
+  var alternativa43 = $("#alternativa4-3").text("DEMO43");
+  var alternativa44 = $("#alternativa4-4").text("DEMO44");
+  var alternativa45 = $("#alternativa4-5").text("DEMO45");
+
+  var alternativa51 = $("#alternativa5-1").text("DEMO51");
+  var alternativa52 = $("#alternativa5-2").text("DEMO52");
+  var alternativa53 = $("#alternativa5-3").text("DEMO53");
+  var alternativa54 = $("#alternativa5-4").text("DEMO54");
+  var alternativa55 = $("#alternativa5-5").text("DEMO");
+
+
+
+
+
   var uniqueId = 1;
   var contador = 0;
 
   //funcion para duplicar preguntas
-  $('.addRow').click(function () {
-    // duplicarAlternativas(unique);
-    duplicar(uniqueId);
-    uniqueId++;
-    contador++;
-    if (contador > 8) {
-      console.log('maximo')
-      $('.addRow').prop('disabled', true);
-    }
-  });
+  /*  $('.addRow').click(function () {
+      // duplicarAlternativas(unique);
+      duplicar(uniqueId);
+      uniqueId++;
+      contador++;
+      if (contador > 8) {
+        console.log('maximo')
+        $('.addRow').prop('disabled', true);
+      }
+    });*/
 
   // $(document).on('change', '[type=radio]', function (e) {});
   $('#close').click(function () {
@@ -179,7 +155,7 @@ $(document).ready(function () {
   });
 });
 
-
+//Crear curso
 $(document).on('click', '#btn-crear', function (event) {
   var ruta = 'https://api-pwcev.herokuapp.com';
 
@@ -194,7 +170,7 @@ $(document).on('click', '#btn-crear', function (event) {
   var curso = {};
   var request = {};
   var id = Cookies.get('id');
-  
+
   curso.centroEstudios = $('#cbo-centro').val().toUpperCase();
   curso.curso = $('#txt-curso').val();
   curso.eap = $('#cbo-eap').val().toUpperCase();
@@ -220,26 +196,256 @@ $(document).on('click', '#btn-crear', function (event) {
   })
 });
 
+//demo
+$(document).on('click', '#btn-demo', function (event) {
 
 
-$('#btn-close').click(function () {
-  deleteCookie();
+  var examen = {};
+  var preguntaObjeto = {};
+  var respuestas = [];
+  var request = {};
+
+  examen.idExamen = null;
+  preguntaObjeto.idPregunta = 0;
+
+  var estadoDefecto = false;
+  var estadoRespuesta = true;
+  var idRespuesta = null;
+  var puntaje = 5;
+  var idPregunta = null;
+
+
+  /*examen y curso */
+
+  var examenes={};
+
+  var curso={};
+
+
+  console.log(examenes);
+
+  var titulo = $("#inputTitulo").val();
+  var fechaInicio = $("#horaInicio").val();
+  var horaInicio =$("#timepicker-two").val();
+  var tiempoDuracion = $("#duracion").val();
+  var cursoId = $("#cbocurso").val();
+
+  examenes.titulo=titulo;
+  examenes.fechaInicio=fechaInicio;
+  examenes.horaInicio= horaInicio;
+  examenes.tiempoDuracion= tiempoDuracion;
+  curso.cursoId= cursoId;
+  examenes.curso = curso;
+
+
+  /* pregunta 1*/
+  var descrip1 = $("#descripcion1").text();
+  var alternativa11 = $("#alternativa1-1").val();
+  var alternativa12 = $("#alternativa1-2").val();
+  var alternativa13 = $("#alternativa1-3").val();
+  var alternativa14 = $("#alternativa1-4").val();
+
+  var preg1 = [descrip1, examen, idPregunta, puntaje];
+  var respuesta1 = [alternativa11, estadoRespuesta, idRespuesta, preguntaObjeto];
+  var respuesta2 = [alternativa12, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta3 = [alternativa13, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta4 = [alternativa14, estadoDefecto, idRespuesta, preguntaObjeto];
+
+  /* pregunta 2 */
+
+  var descrip2 = $("#descripcion2").text();
+  var alternativa21 = $("#alternativa2-1").val();
+  var alternativa22 = $("#alternativa2-2").val();
+  var alternativa23 = $("#alternativa2-3").val();
+  var alternativa24 = $("#alternativa2-4").val();
+
+  var preg2 = [descrip2, examen, idPregunta, puntaje];
+  var respuesta5 = [alternativa21, estadoRespuesta, idRespuesta, preguntaObjeto];
+  var respuesta6 = [alternativa22, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta7 = [alternativa23, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta8 = [alternativa24, estadoDefecto, idRespuesta, preguntaObjeto];
+
+  /* pregunta 3 */
+
+  var descrip3 = $("#descripcion3").text();
+  var alternativa31 = $("#alternativa3-1").val();
+  var alternativa32 = $("#alternativa3-2").val();
+  var alternativa33 = $("#alternativa3-3").val();
+  var alternativa34 = $("#alternativa3-4").val();
+
+  var preg3 = [descrip3, examen, idPregunta, puntaje];
+  var respuesta9 = [alternativa31, estadoRespuesta, idRespuesta, preguntaObjeto];
+  var respuesta10 = [alternativa32, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta11 = [alternativa33, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta12 = [alternativa34, estadoDefecto, idRespuesta, preguntaObjeto];
+
+  /* pregunta 4 */
+
+  var descrip4 = $("#descripcion4").text();
+  var alternativa41 = $("#alternativa4-1").val();
+  var alternativa42 = $("#alternativa4-2").val();
+  var alternativa43 = $("#alternativa4-3").val();
+  var alternativa44 = $("#alternativa4-4").val();
+
+  var preg4 = [descrip4, examen, idPregunta, puntaje];
+  var respuesta13 = [alternativa41, estadoRespuesta, idRespuesta, preguntaObjeto];
+  var respuesta14 = [alternativa42, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta15 = [alternativa43, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta16 = [alternativa44, estadoDefecto, idRespuesta, preguntaObjeto];
+
+  /* pregunta 5*/
+
+  var descrip5 = $("#descripcion5").text();
+  var alternativa51 = $("#alternativa5-1").val();
+  var alternativa52 = $("#alternativa5-2").val();
+  var alternativa53 = $("#alternativa5-3").val();
+  var alternativa54 = $("#alternativa5-4").val();
+
+  var preg5 = [descrip5, examen, idPregunta, puntaje];
+  var respuesta17 = [alternativa51, estadoRespuesta, idRespuesta, preguntaObjeto];
+  var respuesta18 = [alternativa52, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta19 = [alternativa53, estadoDefecto, idRespuesta, preguntaObjeto];
+  var respuesta20 = [alternativa54, estadoDefecto, idRespuesta, preguntaObjeto];
+
+
+  var respuestas = [
+    respuesta1, respuesta2, respuesta3, respuesta4, respuesta5,
+    respuesta6, respuesta7, respuesta8, respuesta9, respuesta10,
+    respuesta11, respuesta12, respuesta13, respuesta14, respuesta15,
+    respuesta16, respuesta17, respuesta18, respuesta19, respuesta20
+  ];
+
+  var preguntas = [
+    preg1, preg2, preg3, preg4, preg5
+  ];
+
+  request.preguntas = preguntas;
+  request.respuestas = respuestas;
+  request.examenes = examenes;
+  console.log(request);
+
 });
 
-$('#tabCurso').click(function () {
-  $('.addRow').prop('disabled', true);
+
+//Crear curso
+$(document).on('click', '#btn-crearExamen', function (event) {
+  var ruta = 'https://api-pwcev.herokuapp.com';
+
+  var examenes = {};
+  var examen = {};
+  var pregunta = {};
+  var request = {};
+
+  examen.idExamen = null;
+  pregunta.idPregunta = 0;
+
+  var estadoDefecto = false;
+  var estadoRespuesta = true;
+  var idRespuesta = null;
+  var puntaje = 4;
+  var idPregunta = null;
+
+  /* pregunta 1*/
+
+  var descrip1 = $("#descripcion1").text();
+  var alternativa11 = $("#alternativa1-1").val();
+  var alternativa12 = $("#alternativa1-2").val();
+  var alternativa13 = $("#alternativa1-3").val();
+  var alternativa14 = $("#alternativa1-4").val();
+
+  var preg1 = [descrip1, examen, idPregunta, puntaje];
+  var respuesta1 = [alternativa11, estadoRespuesta, idRespuesta, pregunta];
+  var respuesta2 = [alternativa12, estadoDefecto, idRespuesta, pregunta];
+  var respuesta3 = [alternativa13, estadoDefecto, idRespuesta, pregunta];
+  var respuesta4 = [alternativa14, estadoDefecto, idRespuesta, pregunta];
+
+  /* pregunta 2 */
+
+  var descrip2 = $("#descripcion2").text();
+  var alternativa21 = $("#alternativa2-1").val();
+  var alternativa22 = $("#alternativa2-2").val();
+  var alternativa23 = $("#alternativa2-3").val();
+  var alternativa24 = $("#alternativa2-4").val();
+
+  var preg2 = [descrip2, examen, idPregunta, puntaje];
+  var respuesta5 = [alternativa21, estadoRespuesta, idRespuesta, pregunta];
+  var respuesta6 = [alternativa22, estadoDefecto, idRespuesta, pregunta];
+  var respuesta7 = [alternativa23, estadoDefecto, idRespuesta, pregunta];
+  var respuesta8 = [alternativa24, estadoDefecto, idRespuesta, pregunta];
+
+  /* pregunta 3 */
+
+  var descrip3 = $("#descripcion3").text();
+  var alternativa31 = $("#alternativa3-1").val();
+  var alternativa32 = $("#alternativa3-2").val();
+  var alternativa33 = $("#alternativa3-3").val();
+  var alternativa34 = $("#alternativa3-4").val();
+
+  var preg3 = [descrip3, examen, idPregunta, puntaje];
+  var respuesta9 = [alternativa31, estadoRespuesta, idRespuesta, pregunta];
+  var respuesta10 = [alternativa32, estadoDefecto, idRespuesta, pregunta];
+  var respuesta11 = [alternativa33, estadoDefecto, idRespuesta, pregunta];
+  var respuesta12 = [alternativa34, estadoDefecto, idRespuesta, pregunta];
+
+  /* pregunta 4 */
+
+  var descrip4 = $("#descripcion4").text();
+  var alternativa41 = $("#alternativa4-1").val();
+  var alternativa42 = $("#alternativa4-2").val();
+  var alternativa43 = $("#alternativa4-3").val();
+  var alternativa44 = $("#alternativa4-4").val();
+
+  var preg4 = [descrip4, examen, idPregunta, puntaje];
+  var respuesta13 = [alternativa41, estadoRespuesta, idRespuesta, pregunta];
+  var respuesta14 = [alternativa42, estadoDefecto, idRespuesta, pregunta];
+  var respuesta15 = [alternativa43, estadoDefecto, idRespuesta, pregunta];
+  var respuesta16 = [alternativa44, estadoDefecto, idRespuesta, pregunta];
+
+  /* pregunta 5*/
+
+  var descrip5 = $("#descripcion5").text();
+  var alternativa51 = $("#alternativa5-1").val();
+  var alternativa52 = $("#alternativa5-2").val();
+  var alternativa53 = $("#alternativa5-3").val();
+  var alternativa54 = $("#alternativa5-4").val();
+
+  var preg5 = [descrip5, examen, idPregunta, puntaje];
+  var respuesta17 = [alternativa51, estadoRespuesta, idRespuesta, pregunta];
+  var respuesta18 = [alternativa52, estadoDefecto, idRespuesta, pregunta];
+  var respuesta19 = [alternativa53, estadoDefecto, idRespuesta, pregunta];
+  var respuesta20 = [alternativa54, estadoDefecto, idRespuesta, pregunta];
+
+
+  var respuestas = [
+    respuesta1, respuesta2, respuesta3, respuesta4, respuesta5,
+    respuesta6, respuesta7, respuesta8, respuesta9, respuesta10,
+    respuesta11, respuesta12, respuesta13, respuesta14, respuesta15,
+    respuesta16, respuesta17, respuesta18, respuesta19, respuesta20
+  ];
+
+  var preguntas = [
+    preg1, preg2, preg3, preg4, preg5
+  ];
+
+  request.preguntas = preguntas;
+  request.respuestas = respuestas;
+  request.examen = examen;
+
+
 });
+
+
 
 $('#tabParam').click(function () {
   Cargar_curso_id('cbocurso', 1);
   var options = {
-    now: "12:35", //hh:mm 24 hour format only, defaults to current time 
+    now: "08:00", //hh:mm 24 hour format only, defaults to current time 
     twentyFour: false, //Display 24 hour format, defaults to false 
     upArrow: 'wickedpicker__controls__control-up', //The up arrow class selector to use, for custom CSS 
     downArrow: 'wickedpicker__controls__control-down', //The down arrow class selector to use, for custom CSS 
     close: 'wickedpicker__close', //The close class selector to use, for custom CSS 
     hoverState: 'hover-state', //The hover state class to use, for custom CSS 
-    title: 'Timepicker', //The Wickedpicker's title, 
+    title: 'Horario', //The Wickedpicker's title, 
     showSeconds: false, //Whether or not to show seconds, secondsInterval: 1, //Change interval for seconds, defaults to 1 , 
     minutesInterval: 1, //Change interval for minutes, defaults to 1 
     beforeShow: null, //A function to be called before the Wickedpicker is shown 
@@ -248,50 +454,9 @@ $('#tabParam').click(function () {
   };
   $('.timepicker').wickedpicker(options);
 
-
-  var titulo = $("#txtTitulo").text();
-  $('#inputTitulo').val(titulo);
-  $('.addRow').prop('disabled', true);
-});
-
-$('#tabExamen').click(function () {
-  $('.addRow').prop('disabled', false);
 });
 
 
-$(document).on('click', '#btn-pregunta', function (event) {
-  $('#inputTituloModal').val($("#txtTitulo").text());
-  $('#modalPregunta').modal('toggle');
-  var id = $(this).attr("btn-pregunta", "descripcion")
-
-  //capturar id
-  var labels = document.getElementsByTagName('label');
-  for (var i = 0; i < labels.length; i++) {
-    if (labels[i].htmlFor != '') {
-      var elem = document.getElementById(labels[i].htmlFor);
-      if (elem)
-        elem.label = labels[i];
-
-    }
-  }
-  var miId = $(labels[1]).attr("id");
-  console.log(miId);
-
-
-
-
-  $(this).attr('valor', 'descripcion');
-
-
-
-
-  //  $(this).attr('id', $(this).attr('id') + uniqueId + '-' + index);
-  //    $(this).attr('name', $(this).attr('name') + uniqueId);
-  //  $(this).attr('value', $(this).attr('value') + uniqueId + '-' + index);
+$('#btn-close').click(function () {
+  deleteCookie();
 });
-
-
-
-// var descripcion = $("#descripcion").text();
-//  $('#tituloDescripcion').text();
-// console.log($("#descripcion").text())
