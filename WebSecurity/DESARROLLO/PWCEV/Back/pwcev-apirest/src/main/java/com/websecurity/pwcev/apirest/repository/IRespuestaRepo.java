3113e9a6eefd8a53b5a09f48d8025a876f81c9a0
Fernando Fuentes <fernando.fuentes@unmsm.edu.pe>
2020-09-06 16:42:49 -0500

    Elimnacion correcta de examenes y cursos

--- WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/repository/IRespuestaRepo.java
+++ WebSecurity/DESARROLLO/PWCEV/Back/pwcev-apirest/src/main/java/com/websecurity/pwcev/apirest/repository/IRespuestaRepo.java
@@ -9,5 +9,5 @@ import com.websecurity.pwcev.apirest.model.Respuesta;
 public interface IRespuestaRepo extends JpaRepository<Respuesta, Integer>{
 	
 	List<Respuesta> findByPreguntaIdPregunta(int idPregunta);
-
+	void deleteByPreguntaIdPregunta(int idPregunta);
 }
