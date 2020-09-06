adbd5f4704f661328d485d7ba70ff5ea0e00fdb1
ManriqueCesar <38671208+ManriqueCesar@users.noreply.github.com>
2020-09-06 02:02:16 -0500

    actualizacion de delete de cookies

--- WebSecurity/DESARROLLO/PWCEV/Front/web/script/cookies.js
+++ WebSecurity/DESARROLLO/PWCEV/Front/web/script/cookies.js
@@ -9,6 +9,13 @@ function deleteCookie() {
     Cookies.remove('nombre');
     Cookies.remove('usuario');
     Cookies.remove('apellido');
+    Cookies.remove('idExamen');
+    Cookies.remove('temp');
+    Cookies.remove('idPregunta1');
+    Cookies.remove('idPregunta2');
+    Cookies.remove('idPregunta3');
+    Cookies.remove('idPregunta4');
+    Cookies.remove('idPregunta5');
 }
 
 function setCookie(cname, cvalue) {
